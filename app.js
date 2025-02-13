12f1415cfd753106f8886c1eff6fa33aa31d43dc
Pablo Henrique <pblohasantos@gmail.com>
2025-02-13 13:13:46 -0300

    Atualizando código JS

--- app.js
+++ app.js
@@ -1,1 +1,36 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let listaDeAmigos = [];
+
+
+//Adiciona os nomes no array
+function adicionarAmigo(){
+    let amigo = document.getElementById('amigo').value;
+    if(amigo == ''){
+        alert('Por favor insira um nome!');
+    }else{
+        listaDeAmigos.push(amigo);    
+        document.getElementById('amigo').value = '';
+        atualizarLista();
+    }
+}
+
+function atualizarLista(){
+    let listarAmigos = document.getElementById('listaAmigos');
+    listarAmigos.innerHTML = '';
+    for(let i = 0; i < listaDeAmigos.length; i++){
+        let itemLista = document.createElement('li');
+        itemLista.innerHTML = listaDeAmigos[i];
+        listarAmigos.appendChild(itemLista);
+        
+    }
+}
+
+function sortearAmigo(){
+
+    if(listaDeAmigos.length == ''){
+        alert('insira pelo menos 2 nomes');
+    }
+    let numero = Math.floor(Math.random()*listaDeAmigos.length+1);
+    let amigoSorteado = listaDeAmigos[numero-1];
+    document.getElementById('resultado').innerHTML =`Seu amigo secreto é ${amigoSorteado}`
+}
+
